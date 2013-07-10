89e74140af6cd45e787cbcf1a6084610fb618a94
Filirom1 <Filirom1@gmail.com>
2013-07-10 13:04:54 +0000

    create a tcp server, not a client

--- template/server.js
+++ template/server.js
@@ -1,12 +1,14 @@
 var net = require('net');
-var client = net.connect({port: process.env.OPENSHIFT_NODEJS_PORT_TCP, host: process.env.OPENSHIFT_NODEJS_IP}, function() { //'connect' listener
-  console.log('client connected');
-  client.write('world!\r\n');
+var conf = {port: process.env.OPENSHIFT_NODEJS_PORT_TCP, host: process.env.OPENSHIFT_NODEJS_IP};
+console.log(conf);
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
 });
-client.on('data', function(data) {
-  client.write(data.toString());
-});
-client.on('end', function() {
-  console.log('client disconnected');
-  client.end();
+server.listen(conf.port, conf.host, function() { //'listening' listener
+  console.log('server bound');
 });
