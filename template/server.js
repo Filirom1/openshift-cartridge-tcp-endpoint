d0324adf1b23e6873bb9d4ae1db6c89a49d94907
Filirom1 <Filirom1@gmail.com>
2013-07-10 13:53:07 +0000

    explain how to connect to the TCP server in the Web Server

--- template/server.js
+++ template/server.js
@@ -1,7 +1,21 @@
 var net = require('net');
-var conf = {port: process.env.OPENSHIFT_NODEJS_PORT_TCP, host: process.env.OPENSHIFT_NODEJS_IP};
-console.log(conf);
-var server = net.createServer(function(c) { //'connection' listener
+var http = require('http');
+var dns = require('dns');
+var os = require('os');
+
+var ip;
+dns.resolve4(os.hostname(), function(err, ips){
+  ip = ips[0];
+});
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('TCP port available at:\n');
+  res.end('telnet  ' + ip + ' ' + process.env.OPENSHIFT_NODEJS_PROXY_PORT_TCP+ '\n');
+}).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
+
+
+var tcpServer = net.createServer(function(c) { //'connection' listener
   console.log('server connected');
   c.on('end', function() {
     console.log('server disconnected');
@@ -9,6 +23,9 @@ var server = net.createServer(function(c) { //'connection' listener
   c.write('hello\r\n');
   c.pipe(c);
 });
-server.listen(conf.port, conf.host, function() { //'listening' listener
+
+tcpServer.listen(process.env.OPENSHIFT_NODEJS_PORT_TCP, process.env.OPENSHIFT_NODEJS_IP, function() { //'listening' listener
   console.log('server bound');
 });
+
+
